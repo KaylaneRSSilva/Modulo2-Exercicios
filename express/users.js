6fe8f8e86a8f27648a35ec0975cbd40aa6531d2f
Kaylane Regina Santos Silva <kaylane.silva@mercadolivre.com>
2022-07-25 17:29:14 -0300

    criando array de usuarios para realizar o exercício assincrono

---
+++ express/users.js
@@ -0,0 +1,16 @@
+let users = [
+  {
+    id: 1,
+    username: 'kaylane',
+    email: 'kaylane@exemplo.com',
+    password: '123456789'
+  },
+  {
+    id: 2,
+    username: 'usuarioOne',
+    email: 'usuarioOne@exemplo.com',
+    password: 'abcdefg'
+  },
+];
+
+module.exports = users;
